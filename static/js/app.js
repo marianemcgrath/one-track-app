3feacac7a62370c2121ccee93028a2ad2ad9061d
Mariane McGrath <marianemcgrath@gmail.com>
2026-05-02 12:43:33 +0000

    Connecting to server

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 // AJAX calls to the Flask API
 
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://mariRmcgrath.pythonanywhere.com";
 
 //------User functions-----------------------------------
 
